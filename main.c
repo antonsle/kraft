65b526f973ddd9fb52b13c15fafad75489eabfe2
Anton Slepnev <anton_sl@inbox.ru>
2014-07-10 23:55:46 +0400

    add special func spc_DSA_sign that must sign our data by DSA algorithm

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
-#include <stdio.h>
-
+#include "stdio.h"
+#include "string.h"
+#include "openssl/dsa.h"
+#include "openssl/engine.h"
+#include "openssl/err.h"
+#include "openssl/md5.h"
+#include <iostream>
+#include <openssl/pem.h>
+#include <openssl/sha.h>
+#include <openssl/ssl.h>
+#include <openssl/evp.h>
+#include <openssl/objects.h>
+int spc_DSA_sign(unsigned char *msg, int msglen, unsigned char *sig, DSA *dsa)
+{
+  unsigned int  ignored;
+  unsigned char hash[20];
+   
+  if (!SHA1(msg, msglen, hash)){ return 0;}
+  
+  return DSA_sign(NID_sha1, hash, 20, sig, &ignored, dsa);  
+}
 int main()
 {
 	printf("hello\n");
