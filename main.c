1d736e53d5ea74c537ecc2c02bfa94cdc8a0d9fc
Anton Slepnev <anton_sl@inbox.ru>
2014-07-11 01:02:57 +0400

    delete debug prints add coments

--- main.c
+++ main.c
@@ -10,96 +10,86 @@
 #include <openssl/ssl.h>
 #include <openssl/evp.h>
 #include <openssl/objects.h>
+using namespace std;
 int i=0;
 int main(int argc,char *argv[])
 {
-	 /*sign -in data.txt -cert cert.pfx -out sign.bin*/
-     char *buff2;
-     buff2=(char*)malloc(8);
-     buff2=argv[3];
-     
-     char *buff3;
-     buff3=(char*)malloc(8);
-     buff3=argv[5];
-      
-     char *buff4;
-     buff4=(char*)malloc(8);
-     buff4=argv[7];
+  
+   
+	/*sign -in data.txt -cert cert.pfx -out sign.bin*/
+	//read main func arguments
+	char *buff2;
+	buff2=(char*)malloc(8);
+	buff2=argv[3];
+	
+	char *buff3;
+	buff3=(char*)malloc(8);
+	buff3=argv[5];
+	
+	char *buff4;
+	buff4=(char*)malloc(8);
+	buff4=argv[7];
+	
 	
 	char *pbkey;
 	pbkey=(char*)malloc(381);
 	char *prkey;
 	prkey=(char*)malloc(403);
 	
-	std::cout<<"programm parametrs:";
-    std::cout<<std::endl<<buff2<<std::endl; 
-    std::cout<<buff3<<std::endl; 
-    std::cout<<buff4<<std::endl; 
-
+	//read from file module
 	FILE *msgf = fopen (buff2,"r+");
 	char message[256*1000];
 	char *newline;
-	
-      while(1)
+	for(i=0;i<2000;i++)
 	{
-      if(fgets(message, 256*1000, msgf) == NULL)
-      
-      if((newline = strchr(message, '\n')) != NULL)
+	if(fgets(message, 256*1000, msgf) == NULL)
+    
+	if((newline = strchr(message, '\n')) != NULL)
       *newline = '\0';
       if(message[i]=='\0')
 	break;
 	}
-	cout<<endl<<message<<endl;
-	   FILE *prkeyf = fopen (buff3,"r");
-		fread(prkey, 403, 1, prkeyf);
+  printf("\nData sucsessfully read from .txt file!");
+    
+    
+    FILE *prkeyf = fopen (buff3,"r");
+    fread(prkey, 403, 1, prkeyf);
    
-	BIO *priv;
-	priv = BIO_new_mem_buf(prkey, -1); 
-	if(priv == NULL){ERR_print_errors_fp(stdout);return 1;}
- cout<<endl<<prkey<<endl; 
+   
+  
+    
+    //key setup module
+    BIO *priv;
+    priv = BIO_new_mem_buf(prkey, -1); 
+    if(priv == NULL){ERR_print_errors_fp(stdout);return 0;}
+
     DSA *private_key;
     private_key = PEM_read_bio_DSAPrivateKey(priv, NULL, NULL, NULL); 
-    if(private_key == NULL){ERR_print_errors_fp(stdout);return 2;} 
-
-    unsigned int result, signlen;
+    if(private_key == NULL){ERR_print_errors_fp(stdout);return 0;} 
+	printf("\nDSA Key sucsessfully read from .pfx file!");
+    unsigned int result, sign_length;
     unsigned char signature[1000] = {0};
 
-   
-    res = DSA_sign(NULL, (const unsigned char*)message, strlen(message),
+   //signature create module
+    result = DSA_sign(NID_sha1, (const unsigned char*)message, strlen(message),
                     (unsigned char*)signature, &sign_length, private_key);
-		FILE *signf;
+
+    if(result!=1){ERR_print_errors_fp(stdout);return 0;}
+
+   
+  
+    
+	//output module
+	FILE *signf;
 	signf=fopen(buff4,"w+"); 
-       fputs((const char*)signature,signf32);
-
-     cout<<endl<<signature<<endl; 
-      char pubkey[] = {
-        "-----BEGIN PUBLIC KEY-----\n"
-        "MIHwMIGoBgcqhkjOOAQBMIGcAkEA3F41fxvcwGZeFxXg2v0/5SR+cxTizT25Qugw\n"
-        "ZgrC7u2zQYTO1Qu0PPDKUrLxkaLzKsUEJbQ1DImnG/FxtRjH7QIVAOp+o1qPhOI4\n"
-        "DtnvYS86ynTxhDcFAkAyKbiUxJigARuuVVlGn4emXOtrT+Al+gmKbbVFfkS62RhS\n"
-        "ZexQ9+mBLv0/1R8Tk37AwuybnflijiPjLxB1ZL00A0MAAkA2DXjw+0PJOyrQfn2Q\n"
-        "44uHyZMG2WfXqT7CIz26ZiIAHDOkZQhOvPLqCKAXfwHgGrgl2JLovhVY8nPMdNk2\n"
-        "vJij\n"
-        "-----END PUBLIC KEY-----"
-        };
-
-    BIO *pub_bio;
-    pub_bio = BIO_new_mem_buf(pubkey, -1); 
-    if(pub_bio == NULL){ERR_print_errors_fp(stdout);return 4;} 
-
-    DSA *public_key;
-    public_key = PEM_read_bio_DSA_PUBKEY(pub_bio, NULL, NULL, NULL);
-    if(public_key == NULL){ERR_print_errors_fp(stdout);return 5;} 
-
-    result = DSA_verify(NULL, (const unsigned char*)message, strlen(message),
-                        signature, sign_length, public_key);
-
-    if(result>0){
-        printf("Verification OK\n");
-    }else{
-        printf("Verification Failure. Error = %i\n",result);
-    }
-	printf("hello\n");
-	getchar();
-  return 0;
-}
+       fputs((const char*)signature,signf);
+       printf("\nDSA signature sucsessfully created!\n");
+    //freedom module
+       
+    BIO_free_all(priv);
+    DSA_free(private_key);
+    fclose(msgf);
+    fclose(signf);
+    fclose(prkeyf);
+    return 0;
+} 
